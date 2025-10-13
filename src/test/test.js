b5fb0e8012569f238c08672476de2bda6aabcd39
muutassim mukhtar <mukhy16@gmail.com>
2025-10-13 11:33:26 +0100

    adde edge case

--- src/test/test.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// test.ts
-const src_1 = require("./src"); // replace with actual exported function
-console.log('SDK test output:', (0, src_1.someFunction)());
